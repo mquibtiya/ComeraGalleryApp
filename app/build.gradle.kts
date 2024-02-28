460eee93be9cca8b7e294ce239c23de2c8e86906
mquibtiya <mariyaquibtiya@gmail.com>
2024-02-29 02:03:26 +0300

    Refactored libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,12 +56,8 @@ android {
 dependencies {
 
     implementation("androidx.core:core-ktx:1.8.10")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
 
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
@@ -81,16 +77,10 @@ dependencies {
     implementation("androidx.test:core-ktx:1.5.0")
     kapt("com.google.dagger:hilt-android-compiler:2.48.1")
 
-    implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
     //Compose
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
-
-    //gson
-    implementation("com.google.code.gson:gson:2.10.1")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
@@ -104,15 +94,14 @@ dependencies {
     // For LiveData testing
     testImplementation("androidx.arch.core:core-testing:2.2.0")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.1")
-    androidTestImplementation("androidx.arch.core:core-testing:2.2.0")
+//    androidTestImplementation("androidx.arch.core:core-testing:2.2.0")
 
 
     //compose
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
+//    debugImplementation("androidx.compose.ui:ui-tooling")
+//    debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
 kapt {
     correctErrorTypes = true
---
+++ app/src/androidTest/java/com/comera/gallery/GalleryScreenTest.kt
@@ -0,0 +1,64 @@
+package com.comera.gallery
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.navigation.NavHostController
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.comera.gallery.domain.MediaItem
+import com.comera.gallery.presentation.ui.composable.GalleryScreen
+import com.comera.gallery.presentation.viewmodel.GalleryViewModel
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mockito.mock
+
+@RunWith(AndroidJUnit4::class)
+class GalleryScreenTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun testGalleryScreenUI() {
+        // Mock data
+        val albums = mapOf<Long, List<MediaItem>>(
+            1L to listOf(mock(MediaItem::class.java)),
+            2L to listOf(mock(MediaItem::class.java))
+            // Add more albums as needed
+        )
+
+        // Launch the GalleryScreen composable
+        composeTestRule.setContent {
+            GalleryScreen(
+                navController = mockNavController(),
+                viewModel = createMockViewModel(albums)
+            )
+        }
+
+        // Check if the app bar is displayed
+        composeTestRule.onNodeWithText("Gallery").assertIsDisplayed()
+
+        // Check if the AlbumItemCard components are displayed
+        albums.keys.forEach { albumId ->
+            onView(withText("Album $albumId")).check(matches(isDisplayed()))
+            // Add more assertions as needed
+        }
+    }
+
+    private fun mockNavController(): NavHostController {
+        val navController = mock(NavHostController::class.java)
+//        every { navController.navigate(any()) } returns Unit
+        return navController
+    }
+
+    private fun createMockViewModel(albums: Map<Long, List<MediaItem>>): GalleryViewModel {
+        val viewModel = mock(GalleryViewModel::class.java)
+//        every { viewModel.albumMap } returns MutableStateFlow(albums.toMutableMap())
+        return viewModel
+    }
+}
